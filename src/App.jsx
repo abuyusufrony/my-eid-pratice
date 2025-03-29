c8afed73d4b860fa4168bb47933d20e7e2fae562
abuyusufrony <abuyusufrony@gmail.com>
2025-03-30 03:25:53 +0600

    3rd-time-fail

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ConditionRendaring from './Components/ConditionRendaring'
+import Todo from './Components/todo/Todo'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,8 +19,11 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <Todo></Todo>
 
       <ConditionRendaring></ConditionRendaring>
+
+
     </>
   )
 }
---
+++ src/Components/todo/Todo.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Todo = () => {
+
+
+    const addbutton = () => {
+        <input type="text" name="name" id="" />
+
+    }
+    return (
+        <div>
+            <h2>Today Heading  </h2>
+        </div>
+    );
+};
+
+export default Todo;
