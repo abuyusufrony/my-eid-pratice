219c2d12efffe4cd5972f7097769f75343c3954e
abuyusufrony <abuyusufrony@gmail.com>
2025-03-31 00:16:42 +0600

    pratice

---
+++ src/Components/EidBeforeNight/EidBeforeNight.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EidBeforeNight = () => {
+    return (
+        <div>
+            <h2>This is from eidBefore Night</h2>
+        </div>
+    );
+};
+
+export default EidBeforeNight;
--- src/Components/todo/Todo.jsx
+++ src/Components/todo/Todo.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import EidBeforeNight from '../EidBeforeNight/EidBeforeNight';
 
 const Todo = () => {
-
+    <input type="text" name="name" id="" />
 
     const addbutton = () => {
-        <input type="text" name="name" id="" />
 
     }
     return (
         <div>
             <h2>Today Heading  </h2>
+
+            <EidBeforeNight></EidBeforeNight>
         </div>
     );
 };
