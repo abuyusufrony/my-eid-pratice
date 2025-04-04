6c0063e2d2d38408fc565ff740a0c73d8197560f
abuyusufrony <abuyusufrony@gmail.com>
2025-04-04 23:55:17 +0600

    git push

--- src/Components/Bookmark/Bookmarks.jsx
+++ src/Components/Bookmark/Bookmarks.jsx
@@ -5,6 +5,7 @@ const Bookmarks = () => {
         <div>
             <h2>this are book mark page</h2>
             <h2>Try untill win </h2>
+            <div className="text-blue-100 bg-amber-500"> stay focus</div>
         </div>
     );
 };
