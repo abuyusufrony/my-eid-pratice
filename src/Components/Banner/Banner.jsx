af3d164ac2685fd67be6cb34ac93cad4412c68a8
abuyusufrony <abuyusufrony@gmail.com>
2025-04-06 21:42:08 +0600

    text-animated-added

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -8,7 +8,11 @@ const Banner = () => {
                 <div className="hero bg-base-200 min-h-screen">
                     <div className="hero-content text-center">
                         <div className="max-w-md">
-                            <h1 className="text-5xl font-bold">Hello there</h1>
+                            <h1 className="text-5xl font-bold">Wellcome <span
+                                class=" font-bold bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text animate-gradient"
+                            >
+                                Rony
+                            </span></h1>
                             <p className="py-6">
                                 Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
                                 quasi. In deleniti eaque aut repudiandae et a id nisi.
