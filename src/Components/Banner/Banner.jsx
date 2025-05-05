b162ee29ff193967db987f3e5a8073567ffd1835
abuyusufrony <abuyusufrony@gmail.com>
2025-05-05 14:39:47 +0600

    banner-clr-added

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -16,6 +16,7 @@ const Banner = () => {
                             <h1 className="text-5xl font-bold">Wellcome <span
                                 class=" font-bold bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text animate-gradient"
                             >
+
                                 Rony
                             </span></h1>
                             <p className="py-6">
